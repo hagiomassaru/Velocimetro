8f0751af662d586c3e54e3c8f6928167b6fe5f22
Massaru <massaruhagiopro@gmail.com>
2023-09-07 01:34:31 +0900

    Colocando dados na lista_parte 1

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const rideListElement = document.querySelector("#ridelist");
 const allRides = getAllRides();
 
-allRides.forEach(([id, value]) => {
+allRides.forEach(async ([id, value]) => {
     const ride = JSON.parse(value);
     ride.id = id;
+
+    const firstPosition = ride.data[0];
+    const firstLocationData = await getLocation(
+        firstPosition.latitude,
+        firstPosition.longitude
+    );
+    console.log(firstLocationData);
     const itemElement = document.createElement("li");
     itemElement.id = ride.id;
-    itemElement.innerText = ride.id;
+
+    const cityDiv = document.createElement("div");
+    cityDiv.innerText = `${firstLocationData.principalSubdivision} - ${firstLocationData.countryCode}`;
+    itemElement.appendChild(cityDiv);
+
+
+    
     rideListElement.appendChild(itemElement);
 });
+
+async function getLocation(latitude, longitude) {
+    const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+
+    const response = await fetch(url);
+    return await response.json();
+}
